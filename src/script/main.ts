aa9f6ed3e049f1783c383c16388d2c9d8439f946
Marine Charra <marine.charra35@gmail.com>
2022-08-03 15:20:42 +0200

    fix viewport size for mobile

--- src/script/main.ts
+++ src/script/main.ts
@@ -14,6 +14,10 @@ function displayModel(id) {
 
 function main() {
     globalThis.defaultRessourcesPath = "default"
+    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+    let vh = window.innerHeight * 0.01;
+    // Then we set the value in the --vh custom property to the root of the document
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
     displayModel(Route.chooseRoute());
 }
 
